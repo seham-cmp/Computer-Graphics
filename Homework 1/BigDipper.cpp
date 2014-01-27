13ea27e64fd4e10bb105f1f2aea3064892037c3b
andrew-kirkham <andrew.kirkham@marquette.edu>
2014-01-26 23:59:17 -0600

    homework 1.1

--- Homework 1/BigDipper.cpp
+++ Homework 1/BigDipper.cpp
@@ -3,75 +3,67 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-#include <fstream>
-
-void drawPolyLineFile(const char * fileName) {
-
-	std::ifstream inStream;
-	inStream.open(fileName);	// open the file
-	if (inStream.fail())
-		return;
-	//glClear(GL_COLOR_BUFFER_BIT);      // clear the screen 
-	GLint numpolys, numLines, x, y;
-	inStream >> numpolys;		           // read the number of polylines
-	for (int j = 0; j < numpolys; j++)  // read each polyline
-	{
-		inStream >> numLines;
-		glBegin(GL_LINE_STRIP);	     // draw the next polyline
-		for (int i = 0; i < numLines; i++)
-		{
-			inStream >> x >> y;        // read the next x, y pair
-			glVertex2i(x, y);
-		}
-		glEnd();
-	}
-	glFlush();
-	inStream.close();
-}
-
-//--------------- setWindow ---------------------
-void setWindow(float left, float right, int bottom, int top)
-{
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(left, right, bottom, top);
-}
-//---------------- setViewport ------------------
-void setViewport(int left, int bottom, int width, int height)
-{
-	glViewport(left, bottom, width, height);
-}
+void display();
+void drawStars();
+void connectTheDots();
 
 
-void render() {
-
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	setWindow(0, 640.0, 0, 480.0);		// set a fixed window
-
-	for (int i = 0; i < 5; i++) {			// for each column
-		for (int j = 0; j < 5; j++)			// for each row
-		{
-			glViewport(i * 64, j * 44, 64, 44); // set the next viewport
-			drawPolyLineFile("dino.dat");		// draw it again
-		}
-	}
-
-	glFlush();
-
-}
-
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Poly Line Demo o_O");
+	glutCreateWindow("Big Dipper");
 
-	glutDisplayFunc(render);;
+	glutDisplayFunc(display);
 	glutMainLoop();
 
 	return(0);
 
+}
+
+void setWindow(float left, float right, int bottom, int top) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(left, right, bottom, top);
+}
+
+
+void display() {
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	setWindow(0, 640.0, 0, 480.0);
+	drawStars();		
+	connectTheDots();
+
+	glFlush();
+
+}
+
+void drawStars() {
+	glPointSize(10); //make points bigger
+	glBegin(GL_POINTS);
+	glVertex2i(82, 480 - 182);
+	glVertex2i(214, 480 - 177);
+	glVertex2i(284, 480 - 228);
+	glVertex2i(373, 480 - 286);
+	glVertex2i(381, 480 - 374);
+	glVertex2i(536, 480 - 400);
+	glVertex2i(574, 480 - 303);
+	glEnd();
+}
+
+void connectTheDots() {
+	glBegin(GL_LINE_STRIP);
+	glVertex2i(82, 480 - 182);
+	glVertex2i(214, 480 - 177);
+	glVertex2i(284, 480 - 228);
+	glVertex2i(373, 480 - 286);
+	glVertex2i(381, 480 - 374);
+	glVertex2i(536, 480 - 400);
+	glVertex2i(574, 480 - 303);
+	glVertex2i(373, 480 - 286);
+	glEnd();
 }
